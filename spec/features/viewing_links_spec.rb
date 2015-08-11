16c4e1c2feb2da96b1f1079bed869e2267231020
Toby Clarke <teeohbee@gmail.com>
2015-08-11 12:02:42 +0100

    Test now adds database entry

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -3,7 +3,7 @@ require './app/data_mapper_setup'
 feature 'Viewing links' do
 
   scenario 'I can see existing links on the links page' do
-    Link.new(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
 
     visit '/links'
 
