3b79db5abbc32904edf0ef1d50e07f3685a0b1a7
raphlevy <raphlevy.perso@gmail.com>
2015-08-12 17:33:01 +0100

    completed add user feature

--- app/app.rb
+++ app/app.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class App < Sinatra::Base
-  enable :session
+  enable :sessions
   set :session_secret, 'super secret'
 
   get '/' do
@@ -38,13 +38,17 @@ class App < Sinatra::Base
     erb :'users/new'
   end
 
-  post'/users/' do
-    User.create(email: params[:email], password: params[:password])
+  post'/users' do
+    user = User.create(email: params[:email], password: params[:password])
     session[:user_id] = user.id
     redirect to('/links')
   end
 
-
+  helpers do
+    def current_user
+      User.get(session[:user_id])
+    end
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -2,9 +2,15 @@ feature 'User sign up' do
 
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
-    expect(page).to have _content('Welcome, alice@example.com')
+    expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
+
+  scenario 'requires a matching confirmation password' do
+    expect { sign_up(password_confirmation: 'wrong')}.not_to change(User; :count)
+    end
+  end
+
 
   def sign_up(email: 'alice@example.com', password: 'oranegs!')
     visit '/users/new'
